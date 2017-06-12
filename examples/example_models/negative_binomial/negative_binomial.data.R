2f99f6ca3dd6cec50188e6e76f57b3b3c8fbd111
Ryan Giordano <rgiordan@gmail.com>
2017-06-12 13:50:53 -0700

    Start package

---
+++ examples/example_models/negative_binomial/negative_binomial.data.R
@@ -0,0 +1,7 @@
+y <- c(0, 1, 4, 0, 2, 2, 5, 0, 1)
+N <- 9
+cauchy_loc_alpha <- 0
+cauchy_scale_alpha <- 10
+cauchy_loc_beta <- 0
+cauchy_scale_beta <- 10
+weights <- rep(1.0, N)
---
+++ examples/run_examples.R
@@ -0,0 +1,47 @@
+library(rstan)
+library(rstansensitivity)
+
+rstan_options(auto_write=TRUE)
+
+# Set this to be the appropriate location of the repo on your computer.
+example_directory <- file.path(
+  Sys.getenv("GIT_REPO_LOC"), "StanSensitivity/examples/example_models")
+
+# Choose the basename of a model that's already been generated.  For details on how to
+# generate a sensitivity model from an existing stan model, see the README in this repo.
+
+model_name <- file.path(example_directory, "normal_censored/normal_censored")
+#model_name <- file.path(example_directory, "negative_binomial/negative_binomial")
+
+# Compile the base model.
+model <- stan_model(paste(model_name, "_generated.stan", sep=""))
+
+# Load the data and hyperparameters.
+stan_data <- new.env()
+source(paste(model_name, "data.R", sep="."), local=stan_data)
+stan_data <- as.list(stan_data)
+
+# Get the posterior draws.
+# Use chains=1 for now to avoid confusion around get_inits.  The script currently assumes
+# the same number of warm-up draws as final samples.
+num_samples <- 5000
+result <- sampling(model, data=stan_data, chains=1, iter=num_samples * 2)
+print(summary(result))
+draws_mat <- extract(result, permute=FALSE)[,1,]
+
+stan_sensitivity_list <- GetStanSensitivityModel(model_name, stan_data)
+sens_result <- GetStanSensitivityFromModelFit(draws_mat, stan_sensitivity_list)
+sens_mat <- sens_result$sens_mat
+
+
+# Inspect the results.
+# Wrapping in a comment block because your analysis may not have all these variables.
+
+# Plot the sensitivity against whatever data value seems relevant (it's not always y).
+weight_rows <- grepl("weights", rownames(sens_mat))
+plot(stan_data$y, sens_mat[weight_rows, 1])
+
+# Look at the sensitivity to other hyperparameters.
+print(result)
+print(sens_mat[!weight_rows, ])
+
